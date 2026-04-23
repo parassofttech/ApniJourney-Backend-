91411fae6124709c62434acfdfbc1a2fa9ef9d74
Paras Sahu <parastop5468@gmail.com>
2026-04-23 21:44:04 +0530

    jhgf

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const cors = require("cors");
 const connectDB = require("./db");
 const contactRouter = require("./routes/contactRouter");
 const tripRoutes = require("./routes/tripRouter");
-const authRouter = require("./routes/authRouter");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-await connectDB()
+connectDB()
 
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
+
 app.use("/api/trips", tripRoutes);
 app.use("/api/contact", contactRouter);
 
