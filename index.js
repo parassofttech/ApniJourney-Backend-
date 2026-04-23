82a53bf98c0e8b99df254d75d1d692d0a6d1357d
Paras Sahu <parastop5468@gmail.com>
2026-04-23 21:07:41 +0530

    fds

--- index.js
+++ index.js
@@ -5,24 +5,17 @@ const cors = require("cors");
 const connectDB = require("./db");
 const contactRouter = require("./routes/contactRouter");
 const tripRoutes = require("./routes/tripRouter");
-const authRouter = require("./routes/authRouter");
-// const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-//  app.use(bodyParser.json());
 connectDB()
-
-app.use(express.json({ limit: "500mb" }));
-app.use(express.urlencoded({ limit: "500mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-app.use("/api/auth",authRouter)
 app.use("/api/trips", tripRoutes);
 app.use("/api/contact", contactRouter);
 
