509e5ae8f9d016781bcc8f16ee709d4496b5c4c6
Paras Sahu <paras2115s@gmail.com>
2026-07-06 08:39:26 +0530

    send amail

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
