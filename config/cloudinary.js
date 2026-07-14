00c93d9205e0aa765bd3ef7991cc964d0608c1b4
Paras Sahu <paras2115s@gmail.com>
2026-07-15 00:21:28 +0530

    resend api

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "a6jz0zw6",
-  api_key: process.env.CLOUDINARY_API_KEY || "147323419669362",
-  api_secret: process.env.CLOUDINARY_API_SECRET  || "Z6FaoZTfZW_QPqVGHw2gzU2qKVc",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET ,
 });
 
 module.exports = cloudinary;
